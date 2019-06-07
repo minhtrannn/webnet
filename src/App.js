a4060e364e2dfb4460a6238efa530661cadc2708
MinhTran <minhmom98@gmail.com>
2019-06-07 20:49:56 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
 import React from 'react';
+import Message from './Message'
 import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 
+
+const Link = (props) => <a href ={props.children} target ="_blank">{props.children}</a>
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+       
     </div>
   );
 }
